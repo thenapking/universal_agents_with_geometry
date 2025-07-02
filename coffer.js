cbb9ee6d5b6cf2dcbfc2a2ec3699c7cd1736c9e8
thenapking <tom.giles@gmail.com>
2025-07-02 13:58:12 +0100

    Different fills

--- coffer.js
+++ coffer.js
@@ -6,9 +6,9 @@
 
 // Set the fill type of each polygon based on its area
 let SMALL =  [ 'solid', 'blank', 'downwards', 'upwards', 'housing', 'vertical', 'dots' ]
-let MEDIUM = [ 'solid', 'blank', 'downwards', 'upwards',   'dots' ] //'circles',
+let TOWN = [ 'blank', 'dots', 'downwards', 'upwards' ] 
 let LARGE =  [ 'blank', 'large-dots', 'dots', 'crosses' ];  
-let COUNTRY = [ 'blank', 'large-dots', 'downwards', 'upwards', 'vertical-dashes', 'horizontal-dashes',  'dots', 'crosses', 'solid' ];
+let COUNTRY = [ 'blank', 'large-dots', 'vertical-dashes', 'horizontal-dashes',  'dots', 'crosses' ];
 let colours = ['brown', 'yellow', 'grey', 'pink', 'orange'] 
 let extended_colours = ['blue', 'red', 'green', 'purple',  'cyan', 'magenta'];
 let all_colours = [...colours, ...extended_colours];
@@ -32,11 +32,31 @@ class Coffer {
     let d = p5.Vector.dist(centroid, this.focus);
     this.fill_type = 'houses'
 
-    if(d > 300 || area > 3000){ this.fill_type = random['blank', 'dots']}
+    if(d > 300 || area > 3000){ this.fill_type = random(COUNTRY)}
+    if(d < 300 && area > CIVIC){ this.fill_type = random(TOWN)}
 
     if(this.fill_type == 'houses'){
       this.fill_object = new Housing(this.polygon);
       this.fill_object.construct();
+    }
+
+    if(this.fill_type == 'dots' || 
+       this.fill_type == 'vertical-dashes' || 
+       this.fill_type == 'horizontal-dashes') {
+      this.fill_object = new Regular(this.polygon, 5, this.fill_type, true);
+      this.fill_object.find_points();
+    }
+
+
+    if(this.fill_type == 'large-dots' || 
+       this.fill_type == 'crosses' ) {
+      this.fill_object = new Regular(this.polygon, 7, this.fill_type, true);
+      this.fill_object.find_points();
+    }
+
+    if(this.fill_type == 'downwards' || this.fill_type == 'upwards') {
+      this.fill_object = new Hatching(this.polygon, 5, this.fill_type);
+      this.fill_object.hatch(this.fill_type);
     }
   }
 
